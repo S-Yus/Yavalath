0d9ea550ddc06d60bd81c3c01376bc56a97419c1
S-Yus <sa2yusei1030@icloud.com>
2025-11-01 23:12:26 +0900

    Yavalath Flask app for Render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="static", static_url_path="")
+app = Flask(__name__)
 
-@app.route("/")
-def root():
-    return send_from_directory("static", "index.html")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
